79d151247e2dd3fc77a622c8fde9a05bbcf460ad
José Barrientos Vela <jbarrientosvela@gmail.com>
2023-01-24 14:06:01 +0100

    add menu en el index.html

--- js/index.js
+++ js/index.js
@@ -1,23 +1,11 @@
-$(document).ready(function() {
-    
-    console.log(ancho);
-  });
-var ancho = $(window).width();
-switch (ancho) {
-    case ancho <= 540:
-        $("#menu-pc").css("display", "none");
-        $("#menu-tablet").css("display", "none");
-        $("#menu-movil").css("display", "flex");
-        break;
-    case ancho <= 768:
-        $("#menu-pc").css("display", "none");
-        $("#menu-tablet").css("display", "flex");
-        $("#menu-movil").css("display", "none");
-        break;
-    case ancho <= 992:
-        $("#menu-pc").css("display", "flex");
-        $("#menu-tablet").css("display", "none");
-        $("#menu-movil").css("display", "none");
-        break;
-    default:
-}
+
+    var btnMenu = $("#btn-menu");
+    var slidebar = document.getElementById("slidevar");
+
+    btnMenu.on("click", () => {
+        if (slidebar.style.getPropertyValue("display") === "none") {
+            slidebar.style.setProperty("display", "block")
+        } else {
+            slidebar.style.setProperty("display", "none")
+        }
+    });
